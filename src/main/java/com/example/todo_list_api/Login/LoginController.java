90bf28ba76db2b801f059b4685df287ea4d99335
Jocelyn Zaman <jzaman@asu.edu>
2024-12-27 16:07:32 -0800

    feat(login user): add login endpoint

---
+++ src/main/java/com/example/todo_list_api/Login/LoginController.java
@@ -0,0 +1,34 @@
+package com.example.todo_list_api.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo_list_api.Registration.InvalidInputException;
+import com.example.todo_list_api.Registration.Token;
+import com.example.todo_list_api.Registration.UserRegistration;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    @Autowired
+    private LoginService service;
+    
+    @PostMapping
+    public ResponseEntity<String> loginUser(@RequestBody UserRegistration userRegistration) {
+        try
+        {
+            // validate login
+            Token token = service.loginUser(userRegistration);
+            return ResponseEntity.status(HttpStatus.OK).body(token.getToken());
+        }
+        catch (InvalidInputException e)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
